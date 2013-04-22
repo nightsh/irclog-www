f5ea9309228d9b781248ceaa44aecdfd3e71abfc
Victor Nițu <victor@debian.org.ro>
2013-04-22 22:37:20 +0300

    Initial import

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Logs</title>
+    <link rel="stylesheet" href="./style.css" />
+</head>
+
+<body>
+    <?php if ($_GET['f'] && file_exists($_GET['f'] . '.log')): ?>
+
+    <div id="log">
+    <h1> <?php echo $_GET['f'].'.log'; ?></h1>
+        <?php echo file_get_contents($_GET['f'] . '.log'); ?>
+    </div>
+
+    <?php else: ?>
+
+    <div id="page">
+
+    <?php
+    foreach (glob('*.log') as $file) {
+        if(!preg_match("/^ChanServ.*/", $file))
+            printf(
+                '<a href="?f=%s" class="log_link">%s</a><br>',
+                str_replace('.log','',$file), $file
+            );
+    }
+    ?>
+
+    </div>
+    <?php endif; ?>
+</body>
+</html>
